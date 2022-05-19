3f59d6489285b46bd9947f9c5542c7877faa303c
asdru333 <asdrubal.villegas.molina@hotmail.com>
2022-05-19 16:30:43 -0600

    primer commit

---
+++ Planetario/Planetario/Models/PagoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Planetario.Models
+{
+    public class PagoModel
+    {
+        public TarjetaModel infoTarjeta { get; set; }
+        public int comprable { get; set; }
+        public int cantidadCompra { get; set; }
+
+        [Display(Name = "Nombre completo del recibidor")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Número de teléfono")]
+        public string Telefono { get; set; }
+
+        [Display(Name = "Dirección de envío")]
+        public string Direccion { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# PI Ingenieria/Bases Grupo-3
+
+Estudiantes: 
+ 
+Daniel Monge Arce B85017 
+
+Eduardo Fernández Baldizón B92911 
+
+Víctor Chacón Barahona B81988 
+
+Asdrúbal Villegas Molina B88583 
+
+Diego Díaz Fonseca B92602 
+
+Bryan Umaña Morales B87997
